8776098d13fc619735a0a0106b5e265f92f2de4b
Jie Xiang <38271942+chia0381@users.noreply.github.com>
2018-06-06 21:57:14 +0800

    Update i2c_accel_oled.ino

--- 050618/i2c_accel_oled.ino
+++ 050618/i2c_accel_oled.ino
@@ -5,17 +5,11 @@
 
 //Import Libraries
 #include <Wire.h>
-#include <SPI.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
 #define OLED_RESET 4
 Adafruit_SSD1306 display(OLED_RESET);
-
-#define NUMFLAKES 10
-#define XPOS 0
-#define YPOS 1
-#define DELTAY 2
 
 #define LOGO16_GLCD_HEIGHT 16 
 #define LOGO16_GLCD_WIDTH  16
